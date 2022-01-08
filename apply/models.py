4cc1b7169133cc41fcb340fef7ce12e125c16221
imlililili <78844463+imlililili@users.noreply.github.com>
2022-01-09 03:37:08 +0800

    Update models.py

--- apply/models.py
+++ apply/models.py
@@ -5,12 +5,11 @@ from django.contrib.auth.models import User     # new
 class Person(models.Model):
 
     VOUCHER_CHOICES = [
-        ('PP', '紙本'),
-        ('CC', '信用卡'),
-        ('ET', '電子票劵'),
-        ('MB', '行動支付')
+        ('PE', '自取'),
+        ('SE', '郵寄'),
     ]
     ssn = models.CharField(max_length=10, blank=False)
+    # amount = models.CharField(max_length=3, blank=False)
     tel = models.CharField(max_length=20, blank=False)
     voucher_id = models.CharField(max_length=2, choices = VOUCHER_CHOICES, default='PP')
     account = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True) # new
